60df7557876a1d2bc7bb5fed82e5cf77c7387b41
linubabu <58581370+linubabu@users.noreply.github.com>
2019-12-06 11:15:40 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<div class="fd">
+  <p> &copy 2018-2019 company.inc </p>
+</div>
+<?php wp_footer(); ?> 
+</body>
+</html>
---
+++ page-gallery.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<?php while(have_posts()): the_post(); ?>
+<div class="gal">
+<?php the_content(); ?>
+</div>
+
+<?php endwhile; ?>
+<?php get_footer(); ?>
